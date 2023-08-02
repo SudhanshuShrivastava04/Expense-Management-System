c64f864bf4eb3e7d1e2b5456718d76e6bae65597
SudhanshuShrivastava04 <211230053@nitdelhi.ac.in>
2023-08-03 03:41:57 +0530

    Add expense or investment form ui

---
+++ src/components/AddForm.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+
+function AddModal() {
+  return (
+    <div className="w-full text-center my-5 border border-gray-200 rounded flex flex-col items-start gap-5 p-3">
+      <Formik
+        initialValues={{ amount: "", description: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form className="flex flex-col items-start w-full gap-4">
+          <Field
+            name="amount"
+            type="number"
+            placeholder="Amount"
+            className="p-2 border border-gray-100 rounded w-full"
+          />
+          <Field
+            name="description"
+            type="text"
+            placeholder="Description"
+            className="p-2 border border-gray-100 rounded w-full"
+          />
+          <div className="flex flex-row gap-5">
+            <label className="flex flex-row items-center gap-1">
+              <Field name="add" type="radio" />
+              Investment
+            </label>
+            <label className="flex flex-row items-center gap-1">
+              <Field name="add" type="radio"  />
+              Expense
+            </label>
+          </div>
+          <button className="bg-primaryHeading p-2 rounded-md text-xl hover:bg-gray-700 text-white self-center">Add Transaction</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default AddModal;
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import {AiOutlinePlus} from 'react-icons/ai';
+import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import AddForm from "./AddForm";
 function Overview() {
-  const[isAddTxnVisible, toggleAddTxn] = useState(false);
+  const [isAddTxnVisible, toggleAddTxn] = useState(false);
   const handleAddTxn = () => {
     toggleAddTxn(!isAddTxnVisible);
-  }
+  };
   return (
-    <div className="w-full flex flex-row items-center font-montserrat my-10">
+    <div className="w-full flex flex-col items-center font-montserrat my-10">
       <div className="flex flex-row w-full items-stretch justify-between">
         <div className="w-1/2 flex flex-row gap-2">
           <h1 className="text-2xl text-primaryHeading font-semibold">
@@ -14,10 +15,21 @@ function Overview() {
           </h1>
           <p className="text-2xl text-primaryHeading font-semibold">$100</p>
         </div>
-        <button className="text-white bg-primaryHeading px-5 rounded-md text-xl hover:bg-gray-700 inline-flex items-center gap-2 duration-300" onClick={handleAddTxn}>
+        <button
+          className="text-white bg-primaryHeading px-5 rounded-md text-xl hover:bg-gray-700 inline-flex items-center gap-2 duration-300"
+          onClick={handleAddTxn}
+        >
           {isAddTxnVisible ? "Close" : "Add"}
-          <AiOutlinePlus style={{ transform: isAddTxnVisible ? "rotate(225deg)" : "", transition: "all 0.3s"}} />
+          <AiOutlinePlus
+            style={{
+              transform: isAddTxnVisible ? "rotate(225deg)" : "",
+              transition: "all 0.3s",
+            }}
+          />
         </button>
+      </div>
+      <div className="w-full overflow-hidden" style={{ height: isAddTxnVisible ? "250px" : "0", transition: "all 0.3s" }}>
+        <AddForm />
       </div>
     </div>
   );
