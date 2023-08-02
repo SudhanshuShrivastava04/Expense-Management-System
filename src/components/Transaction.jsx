92312f644d0d5ba01a4665b788b8e8dfb86bfdbd
SudhanshuShrivastava04 <211230053@nitdelhi.ac.in>
2023-08-03 03:44:39 +0530

    under process tag

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,5 +1,5 @@
 function Transaction() {
-  return <div className="w-full flex flex-col items-center font-montserrat">Transaction</div>;
+  return <div className="w-full flex flex-col items-center font-montserrat">Under Progress</div>;
 }
 
 export default Transaction;
